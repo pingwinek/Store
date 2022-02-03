c6662f2cca6e850b11ab3cfcba6737604c53561e
pingwinek <devmaterials@gmail.com>
2022-02-03 15:14:05 +0100

    add test class

--- Store/UnitTests/PageLinkTagHelperTests.cs
+++ Store/UnitTests/PageLinkTagHelperTests.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moq;
+using MVC.Infrastructure;
+using MVC.Models.ViewModels;
 using Xunit;
 
 namespace UnitTests
@@ -20,6 +25,33 @@ namespace UnitTests
             var urlHelperFactory = new Mock<IUrlHelperFactory>();
             urlHelperFactory.Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>()))
                 .Returns(urlHelper.Object);
+
+            PageLinkTagHelper helper = new PageLinkTagHelper(urlHelperFactory.Object) {
+                PageModel = new PagingInfo {
+                    CurrentPage = 2,
+                    TotalItems = 28,
+                    ItemsPerPage = 10
+                },
+                PageAction = "Test"
+            };
+
+            TagHelperContext ctx = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), ""
+            );
+
+            var content = new Mock<TagHelperContent>();
+            TagHelperOutput output = new TagHelperOutput("div", new TagHelperAttributeList(),
+                        (cache, encoder) => Task.FromResult(content.Object));
+
+            // Act
+            helper.Process(ctx, output);
+
+            // Assert
+            Assert.Equal(@"<a href=""Test/Page1"">1</a>" + 
+                         @"<a href=""Test/Page2"">2</a>" +
+                         @"<a href=""Test/Page3"">3</a>",
+                         output.Content.GetContent());
         }
     }
 }
